cb0f2ba27cbd8fa24c37c563404adad87a58cca8
VirajDilshanKumarage <viraj123e@gmail.com>
2025-07-16 23:11:59 +0530

    Modification: dev- applying themes in app

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:dio/dio.dart';
 import 'package:hive_flutter/adapters.dart';
+import 'package:job_list/core/theme/app_theme.dart';
 import 'package:job_list/data/datasources/job_remote_data_source_implementation.dart';
 import 'package:job_list/data/models/job_model.dart';
 import 'package:job_list/data/repositories/job_repository_implementation.dart';
 import 'package:job_list/domain/usecases/get_jobs.dart';
 import 'package:job_list/presentation/bloc/favorite_jobs/favorite_bloc.dart';
 import 'package:job_list/presentation/bloc/job/job_bloc.dart';
+import 'package:job_list/presentation/bloc/theme/theme_bloc.dart';
 import 'package:job_list/presentation/screens/favorite_jobs.dart';
 import 'package:job_list/presentation/screens/job_list_screen.dart';
 
@@ -28,6 +30,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
+        BlocProvider(create: (context) => ThemeBloc()),
         BlocProvider(
           create:
               (context) => JobBloc(
@@ -42,12 +45,18 @@ class MyApp extends StatelessWidget {
           create: (context) => FavoriteBloc(favoritesBox: favoritesBox),
         ),
       ],
-      child: MaterialApp(
-        title: 'Job List App',
-        theme: ThemeData(primarySwatch: Colors.blue),
-        home: const JobListPage(),
-        debugShowCheckedModeBanner: false,
-        routes: {'/favorites': (context) => const FavoritesPage()},
+      child: BlocBuilder<ThemeBloc, ThemeState>(
+        builder: (context, themeState) {
+          return MaterialApp(
+            title: 'Job Listing App',
+            theme: AppTheme.lightTheme,
+            darkTheme: AppTheme.darkTheme,
+            themeMode: themeState.themeMode, 
+            home: const JobListPage(),
+            debugShowCheckedModeBanner: false,
+            routes: {'/favorites': (context) => const FavoritesPage()},
+          );
+        },
       ),
     );
   }
