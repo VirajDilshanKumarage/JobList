e4450315f349f114f2963e264526da088a4110d2
VirajDilshanKumarage <viraj123e@gmail.com>
2025-07-16 19:26:30 +0530

    New Feature: dev- create jobmodel to save data and read data from hive, and logic for converting data between JSON to business logic, and genarate hive instructions

---
+++ lib/data/models/job_model.dart
@@ -0,0 +1,126 @@
+
+import 'package:equatable/equatable.dart';
+import 'package:hive/hive.dart';
+import 'package:job_list/domain/entities/job.dart';
+
+part 'job_model.g.dart';
+
+@HiveType(typeId: 0)
+class JobModel extends Equatable {
+  @HiveField(0)
+  final String id;
+  @HiveField(1)
+  final String title;
+  @HiveField(2)
+  final String company;
+  @HiveField(3)
+  final String location;
+  @HiveField(4)
+  final String type;
+  @HiveField(5)
+  final String description;
+  @HiveField(6)
+  final String salary;
+  @HiveField(7)
+  bool isFavorite;
+
+  Job toEntity() {
+    return Job(
+      id: id,
+      title: title,
+      company: company,
+      location: location,
+      type: type,
+      description: description,
+      salary: salary,
+      isFavorite: isFavorite,
+    );
+  }
+
+  JobModel({
+    required this.id,
+    required this.title,
+    required this.company,
+    required this.location,
+    required this.type,
+    required this.description,
+    required this.salary,
+    this.isFavorite = false,
+  });
+
+
+  JobModel copyWith({
+  String? id,
+  String? title,
+  String? company,
+  String? location,
+  String? type,
+  String? description,
+  String? salary,
+  bool? isFavorite,
+}) {
+  return JobModel(
+    id: id ?? this.id,
+    title: title ?? this.title,
+    company: company ?? this.company,
+    location: location ?? this.location,
+    type: type ?? this.type,
+    description: description ?? this.description,
+    salary: salary ?? this.salary,
+    isFavorite: isFavorite ?? this.isFavorite,
+  );
+}
+
+
+  
+
+  factory JobModel.fromJson(Map<String, dynamic> json) {
+    return JobModel(
+      id: json['id'] ?? '',
+      title: json['title'] ?? '',
+      company: json['company'] ?? '',
+      location: json['location'] ?? '',
+      type: json['type'] ?? '',
+      description: json['description'] ?? '',
+      salary: json['salary'] ?? 'Not specified',
+      isFavorite: json['isFavorite'] ?? false,
+    );
+  }
+
+
+    factory JobModel.fromEntity(Job job) {
+    return JobModel(
+      id: job.id,
+      title: job.title,
+      company: job.company,
+      location: job.location,
+      type: job.type,
+      description: job.description,
+      salary: job.salary,
+      isFavorite: job.isFavorite,
+    );
+  }
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'title': title,
+        'company': company,
+        'location': location,
+        'type': type,
+        'description': description,
+        'salary': salary,
+        'isFavorite': isFavorite,
+      };
+
+  @override
+  List<Object?> get props => [
+        id,
+        title,
+        company,
+        location,
+        type,
+        description,
+        salary,
+        isFavorite,
+      ];
+}
---
+++ lib/data/models/job_model.g.dart
@@ -0,0 +1,62 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'job_model.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class JobModelAdapter extends TypeAdapter<JobModel> {
+  @override
+  final int typeId = 0;
+
+  @override
+  JobModel read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return JobModel(
+      id: fields[0] as String,
+      title: fields[1] as String,
+      company: fields[2] as String,
+      location: fields[3] as String,
+      type: fields[4] as String,
+      description: fields[5] as String,
+      salary: fields[6] as String,
+      isFavorite: fields[7] as bool,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, JobModel obj) {
+    writer
+      ..writeByte(8)
+      ..writeByte(0)
+      ..write(obj.id)
+      ..writeByte(1)
+      ..write(obj.title)
+      ..writeByte(2)
+      ..write(obj.company)
+      ..writeByte(3)
+      ..write(obj.location)
+      ..writeByte(4)
+      ..write(obj.type)
+      ..writeByte(5)
+      ..write(obj.description)
+      ..writeByte(6)
+      ..write(obj.salary)
+      ..writeByte(7)
+      ..write(obj.isFavorite);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is JobModelAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
