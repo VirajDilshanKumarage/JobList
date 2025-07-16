e539a380a384d70df9ea3077c14e88b86b742a6a
VirajDilshanKumarage <viraj123e@gmail.com>
2025-07-16 22:44:51 +0530

    Add: dev- adding themes

---
+++ lib/core/theme/app_theme.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static final ThemeData lightTheme = ThemeData(
+    primarySwatch: Colors.blue,
+    brightness: Brightness.light,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Colors.blue,
+      foregroundColor: Colors.white,
+    ),
+  );
+
+  static final ThemeData darkTheme = ThemeData(
+    primarySwatch: Colors.blueGrey,
+    brightness: Brightness.dark,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Colors.blueGrey,
+      foregroundColor: Colors.white,
+    ),
+  );
+}
